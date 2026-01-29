384f5cd112bda5375e6627e8e58c750e905e666f
ben <ebrahimndille@gmail.com>
2026-01-29 12:00:09 -0800

    create the sql query to create the db tables

---
+++ backend/schoolerp.sql
@@ -0,0 +1,64 @@
+use schoolerp;
+go
+
+create table User_table (
+     User_id int Not null PRIMARY KEY,
+     Name varchar(255),
+     Email varchar(255),
+     Password varchar(255),
+     Role varchar(255),
+     Created_at varchar(255)
+);
+
+create table Student_table (
+     Student_id int not null primary key,
+     User_id int not null foreign key references user_table(User_id),
+     First_name varchar(255),
+     Last_name varchar(255),
+     Matricule int not null,
+     Department varchar(255),
+     Level varchar(255),
+     Phone int,
+     Address varchar(255)
+);
+
+create table Teacher_table (
+      Teacher_id int not null primary key,
+      User_id int not null foreign key references user_table(User_id),
+      First_Name varchar(255),
+      Last_name Varchar(255),
+      Department varchar(255) 
+);
+
+create table Courses_table (
+      Course_id int not null primary key,
+      Course_name varchar(255),
+      Credits varchar(255),
+      Scores int not null,
+      grade varchar(255),
+      Semester varchar(255),
+      Teacher_id int foreign key references Teacher_table(Teacher_id)
+);
+
+create table Result_table (
+      Result_id int not null primary key,
+      Student_id int foreign key references Student_table(student_id),
+      Course_id int foreign key references Courses_table(Course_id),
+      Scores int not null,
+      Grade varchar(255),
+      Semester Varchar(255),
+);
+
+create table Payment_table (
+      Payment_id int not null primary key,
+      student_id int not null foreign key references Student_table(Student_id),
+      Amount int not null,
+      Date Date,
+      Status Varchar(255)
+);
+
+create table Annoucement_table(
+      Annoucement_id int not null primary key,
+      Title varchar(255),
+      Message varchar(max)
+);
--- frontend/index.js
+++ frontend/index.js
@@ -5,11 +5,11 @@ const register_btn = document.getElementById('register-btn')
 const login_btn = document.getElementById('login-btn');
 
 register_btn.addEventListener('click', () => {
-    register.style.display = 'block';
+    register.style.display = 'flex';
     loginform.style.display = 'none';
 })
 
 login_btn.addEventListener('click', () => {
-       loginform.style.display = 'block';
+       loginform.style.display = 'flex';
        register.style.display = 'none';
 });
